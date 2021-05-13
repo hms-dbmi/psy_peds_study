93ec1331904f281d2777fb234084a0f629e05046
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2021-05-13 13:47:47 -0400

    sql code updated and jupyter notebook with the step by step analysis to generate the plots

--- sql/bch_mental_health_table.sql
+++ sql/bch_mental_health_table.sql
@@ -1,17 +1,25 @@
-CREATE TABLE ase_mental_health
+-- table including only primary diagnosis, inpatients from 11 to 17 y/o
+CREATE TABLE mental_health_primary
 AS SELECT obs.patient_num, obs.concept_cd, obs.start_date, pat.birth_date, floor(months_between(obs.start_date, pat.birth_date) / 12) as age, 
-         peds_codes.mental_health_disorder_group,  peds_codes.description
+         peds_codes.mental_health_disorder_group,  peds_codes.description, obs.modifier_cd, vis.inout_cd
 FROM observation_fact obs
 INNER JOIN patient_dimension pat ON obs.patient_num=pat.patient_num
+INNER JOIN visit_dimension vis ON obs.encounter_num= vis.encounter_num
 INNER JOIN ASE_PEDS_ICD_CODES peds_codes ON obs.concept_cd=peds_codes.ICD10_CODE
-WHERE     obs.start_date > '21-04-2020'
-      AND floor(months_between(obs.start_date, pat.birth_date) / 12) > 10 
-      AND floor(months_between(obs.start_date, pat.birth_date) / 12) < 18;
-SELECT COUNT(*) as nrows 
-FROM ase_mental_health
-;
+WHERE floor(months_between(obs.start_date, pat.birth_date) / 12) > 10 
+      AND floor(months_between(obs.start_date, pat.birth_date) / 12) < 18 
+      AND lower(vis.inout_cd) = 'inpatient' 
+      AND obs.modifier_cd = 'ICD:P';
 
-
-SELECT COUNT(*) as nrows 
-FROM ase_mental_health
-;
+-- table including only primary and secondary diagnosis, inpatients from 11 to 17 y/o
+CREATE TABLE mental_health_icdPandS
+AS SELECT obs.patient_num, obs.concept_cd, obs.start_date, pat.birth_date, floor(months_between(obs.start_date, pat.birth_date) / 12) as age, 
+         peds_codes.mental_health_disorder_group,  peds_codes.description, obs.modifier_cd, vis.inout_cd
+FROM observation_fact obs
+INNER JOIN patient_dimension pat ON obs.patient_num=pat.patient_num
+INNER JOIN visit_dimension vis ON obs.encounter_num= vis.encounter_num
+INNER JOIN ASE_PEDS_ICD_CODES peds_codes ON obs.concept_cd=peds_codes.ICD10_CODE
+WHERE floor(months_between(obs.start_date, pat.birth_date) / 12) > 10 
+      AND floor(months_between(obs.start_date, pat.birth_date) / 12) < 18 
+      AND lower(vis.inout_cd) = 'inpatient' 
+      AND obs.modifier_cd in ('ICD:P', 'ICD:S');
